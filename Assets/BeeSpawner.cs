e59c6e4779b56ffffd0c96b8cb819afb2cbdacca
unknown <C13547287@KSTa305-14.ict.ad.dit.ie>
2017-05-19 09:57:27 +0100

    Spawn bees on coroutine

---
+++ Assets/BeeSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeeSpawner : MonoBehaviour
+{
+    public GameObject beePrefab;
+
+    private int numBees = 0;
+    private float pollenCount = 10.0f;
+
+	void Start ()
+    {
+        StartCoroutine(SpawnBee());
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    public IEnumerator SpawnBee()
+    {
+        while (true)
+        {
+            if (numBees < 10)
+            {
+                Instantiate(beePrefab);
+            }
+
+            yield return new WaitForSeconds(2.0f);
+        }
+    }
+}
